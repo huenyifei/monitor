02c73b188efebfa98882bfbda472885dc799e29b
月影斜 <huenyifei@126.com>
2017-07-06 13:45:13 +0800

    Init project

--- taste-oracle.js
+++ taste-oracle.js
@@ -1,2 +1,7 @@
-var oracledb = require('oracledb')
-console.log('hello, world')
+/**
+ * Module   : taste-oracle
+ * Function : 了解oracledb的各种用法
+ * Project  : monitor
+ * License  : Copyright © 2017 Shanghai Zhenhua Heavy Industries Co., Ltd. All rights reserved
+ * Author   : Create by Colin Wu on 2017/7/6
+ */
